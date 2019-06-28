5b185cb8ad12b76cd591ef3ea4936ab631f9f6f7
赵文一 <zhaowenyi17@163.com>
2019-06-28 09:15:24 +0800

    111111111111111111111

--- cv_py/imtools.py
+++ cv_py/imtools.py
@@ -1,10 +1,16 @@
 import os
 from PIL import Image
 import numpy as np
+import cv2
 import os
 
 
 def get_imlist(path):
     """返回目录中所有JPG图像的文件名列表"""
-    return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg')]
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg')]
 
+
+def imresize(im, sz):
+    """使用PIL重新定义图像数组的大小"""
+    pil_im = Image.fromarray(im)
+    return np.array(pil_im.resize(sz))
