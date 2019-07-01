a8bd0fcd80141abf8027e0144cbfb5df5f9a70b5
赵文一 <zhaowenyi17@163.com>
2019-07-01 17:45:02 +0800

    原始数据和滤波数据都显示

--- visualLocMySQL.py
+++ visualLocMySQL.py
@@ -10,14 +10,16 @@ connection = pymysql.connect(host='localhost',
                              cursorclass=pymysql.cursors.DictCursor)
 try:
     with connection.cursor() as cursor:
-        sql = "SELECT `time`, `z_corrected` FROM `axis`"
+        sql = "SELECT `time`, `z` FROM `axis`"
+        # sql = "SELECT `time`, `z_corrected` FROM `axis`"
         cursor.execute(sql)
         result = cursor.fetchall()
         xAxis = []
         yAxis = []
         for singleRec in result:
             xAxis.append(singleRec['time'])
-            yAxis.append(singleRec['z_corrected'])
+            yAxis.append(singleRec['z'])
+            # yAxis.append(singleRec['z_corrected'])
         print(result)
         print(xAxis)
         x = [n for n in range(1, len(xAxis) + 1)]
@@ -29,5 +31,37 @@ try:
         plt.ylabel(u'竖直方向坐标/mm', fontproperties="SimHei")
         plt.xlabel(u'采样时间', fontproperties="SimHei")
         plt.savefig('E:/out/axis_z_' + xAxis[0] + '.png')
+finally:
+    connection.close()
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='root',
+                             db='visualloc',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+try:
+    with connection.cursor() as cursor:
+        # sql = "SELECT `time`, `z` FROM `axis`"
+        sql = "SELECT `time`, `z_corrected` FROM `axis`"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        xAxis = []
+        yAxis = []
+        for singleRec in result:
+            xAxis.append(singleRec['time'])
+            # yAxis.append(singleRec['z'])
+            yAxis.append(singleRec['z_corrected'])
+        print(result)
+        print(xAxis)
+        x = [n for n in range(1, len(xAxis) + 1)]
+        xMat = np.mat(x).T
+        print(yAxis)
+        plt.figure()
+        plt.plot(xMat, yAxis)
+        plt.title(u'目标点位移时程曲线', fontproperties="KaiTi", fontsize=20)
+        plt.ylabel(u'竖直方向坐标/mm', fontproperties="SimHei")
+        plt.xlabel(u'采样时间', fontproperties="SimHei")
+        plt.savefig('E:/out/axis_z_corrected_' + xAxis[0] + '.png')
 finally:
     connection.close()
--- visualLocalization.py
+++ visualLocalization.py
@@ -49,7 +49,7 @@ with open(current_path + './test_dir/' + 'fpsRecord_test.yml', 'r') as f:
     plt.title(u'目标点位移时程曲线', fontproperties="KaiTi", fontsize=20)
     plt.ylabel(u'竖直方向坐标/mm', fontproperties="SimHei")
     plt.xlabel(u'采样时间', fontproperties="SimHei")
-    plt.savefig('E:/out/axis_z_' + name + '.png')
+    plt.savefig('E:/out/axis_z_kalman_' + name + '.png')
 
     # h = axis_mat[:, 0] * 1000  # 转化为毫米
     # h_0 = np.max(h)  # 零位（最高点）
@@ -61,5 +61,44 @@ with open(current_path + './test_dir/' + 'fpsRecord_test.yml', 'r') as f:
     # plt.ylabel(u'水平方向坐标/mm', fontproperties="SimHei")
     # plt.xlabel(u'时间/s', fontproperties="SimHei")
     # plt.savefig("axis_x.png")
+
+with open(origin_path, 'r') as originf:
+    origin_data = originf.read()
+    subYaml = origin_data.split('\n')
+    with open(current_path + './test_dir/' + 'fpsRecord_test.yml', 'w') as wf:
+        i = 2
+        while i < len(subYaml):
+            if subYaml[i] != '---' and subYaml[i] != '...':
+                wf.write(subYaml[i] + '\n')
+            i += 1
+
+with open(current_path + './test_dir/' + 'fpsRecord_test.yml', 'r') as f:
+    temp = yaml.load(f.read())
+    # temp = yaml.load(f.read(), Loader=yaml.safe_load)
+    temp = collections.OrderedDict(temp)
+    keys = list(temp.keys())
+    values = temp.values()
+    axis = []
+    for val in values:
+        axis.append(val)
+
+    # print(axis)
+    # print(np.shape(axis))
+    name = keys[0].split("_")[1]
+    axis_mat = np.mat(axis)
+    z = axis_mat[:, 2] * 1000  # 转化为毫米
+    z_0 = np.max(z)  # 零位（最高点）
+    z_max_axis = np.min(z)  # 最大挠度（最低点）
+    z_max_trans = z_0 - z_max_axis  # 最大位移（极差）
+
+    x = [n for n in range(1, z.shape[0] + 1)]
+    xMat = np.mat(x).T  # 数组转矩阵，数值操作
+
+    plt.figure()
+    plt.plot(xMat, z - z_0)
+    plt.title(u'目标点位移时程曲线', fontproperties="KaiTi", fontsize=20)
+    plt.ylabel(u'竖直方向坐标/mm', fontproperties="SimHei")
+    plt.xlabel(u'采样时间', fontproperties="SimHei")
+    plt.savefig('E:/out/axis_z_origin_' + name + '.png')
 
 os.rename(kalman_path, 'E:/out/fpsRecord_kalman_' + name + '.yml')
